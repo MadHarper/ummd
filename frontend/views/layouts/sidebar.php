b68ce9b7e9f0e917b185e5fc65b901bb7976f06b
Колесников Алексей Юльевич <aleksey.kolesnikov@fire.iac.spb.ru>
2018-03-21 15:16:31 +0300

    CRUD для Типа докуменов

--- frontend/views/layouts/sidebar.php
+++ frontend/views/layouts/sidebar.php
@@ -17,6 +17,7 @@
                         ['label' => 'Страны', 'icon' => 'fa fa-globe', 'url' => ['/catalog/country/index'], 'visible' => true],
                         ['label' => 'Организации', 'icon' => 'fa fa-university', 'url' => ['/catalog/organization/index'], 'visible' => true],
                         ['label' => 'Должностные лица', 'icon' => 'fa fa-address-book-o', 'url' => ['/catalog/employee/index'], 'visible' => true],
+                        ['label' => 'Типы документов', 'icon' => 'fa fa-address-book-o', 'url' => ['/catalog/document-type/index'], 'visible' => true],
                     ]],
                     ['label' => 'Соглашения', 'icon' => 'fa fa-handshake-o', 'url' => ['/agreement/default/index'], 'visible' => true],
                     ['label' => 'Документы', 'icon' => 'fa fa-file-word-o', 'url' => ['/agreement/document/list'], 'visible' => true],
